b40aa73091830f33c7f086eda87221fe93929a0a
Carlos Garcia <agzsoftsi@gmail.com>
2021-04-03 22:11:06 -0500

    Update script-compiler.js

--- 18-Cases/1-example/script-compiler.js
+++ 18-Cases/1-example/script-compiler.js
@@ -1,25 +1,27 @@
 /**
   * Case 1 - Create a program to proces two variables and return the result
   */
+// eslint-disable-next-line no-unused-vars
 function sum(a, b) {
   document.getElementById('result').innerText = (Number(a) + Number(b));
   return a + b;
-};
-
+}
+// eslint-disable-next-line no-unused-vars
 function res(a, b) {
   document.getElementById('result').innerText = (Number(a) - Number(b));
   return a - b;
-};
-
+}
+// eslint-disable-next-line no-unused-vars
 function mul(a, b) {
   document.getElementById('result').innerText = (Number(a) * Number(b));
   return a * b;
-};
+}
 
+// eslint-disable-next-line no-unused-vars
 function div(a, b) {
   if (b === 0) {
     return 'Math Error';
   }
   document.getElementById('result').innerText = (Number(a) / Number(b));
   return a / b;
-};
+}
